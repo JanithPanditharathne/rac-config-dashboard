9ab4254d5a22f317839a165c2142557eea536ed2
Sandaru Kalapuge <sandaruk@zone24x7.com>
2020-06-10 11:17:12 +0530

    Modify keycloak configs.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { KeycloakConfig } from 'keycloak-angular';
 
 const keycloakConfig: KeycloakConfig = {
   url: 'http://10.101.16.79:8080/auth',
-  realm: 'RAC Visualisation',
+  realm: 'Configuration Portal',
   clientId: 'ibrac-config-portal-admin'
 };
 
